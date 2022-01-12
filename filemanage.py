e2c3409622e03e9895a0bfab7c3f83348578f834
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2022-01-12 10:19:11 +0530

    Delete filemanage.py

--- filemanage.py
+++
@@ -1,9 +0,0 @@
-def file_attr(fname):                    # takes filename as attribute
-    with open(fname, 'w') as newfile:       # fname as first attribute and in write mode
-        newfile.write("Hey this is PFSD assingment")     # write the contents to file
-        newfile.write("This is a new file")             # another content
-    s = open(fname)                           # opening file name for terminal
-    print(s.read())                            # reading the contents from that file and displaying it
-
-
-file_attr('PFSDf1.txt')                  # passing filename
