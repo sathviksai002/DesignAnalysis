bc26619376eb83799832d4384a13f0bb8c4e2ea2
SAI SATHVIK <sathviksai002@gmail.com>
2022-01-17 11:57:29 +0530

    version 1

--- mergesort.py
+++ mergesort.py
@@ -1,27 +1,32 @@
 def merge_sort(arr):
-    left_array = arr[:len(arr)//2]
-    right_array = arr[len(arr)//2:]
+    if len(arr) > 1:
+        left_array = arr[:len(arr) // 2]
+        right_array = arr[len(arr) // 2:]
 
-    i = 0     # index for left array left element
-    j = 0     # index for right array left element
-    k = 0     # index for merged array elements
+        # recursion to call the array until it reaches the smallest
+        merge_sort(left_array)
+        merge_sort(right_array)
 
-    while i < len(left_array) and j < len(right_array):
-        if left_array[i] < right_array[j]:
+        i = 0  # index for left array left element
+        j = 0  # index for right array left element
+        k = 0  # index for merged array elements
+
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                arr[k] = left_array[i]
+                i += 1
+            else:
+                arr[k] = right_array[j]
+                j += 1
+            k += 1
+        while i < len(left_array):
             arr[k] = left_array[i]
             i += 1
-        else:
+            k += 1
+        while j < len(right_array):
             arr[k] = right_array[j]
             j += 1
-        k += 1
-    while i < len(left_array):
-        arr[k] = left_array[i]
-        i += 1
-        k += 1
-    while j < len(right_array):
-        arr[k] = right_array[j]
-        j += 1
-        k += 1
+            k += 1
 
 
 print("Enter the list of elements: ")
